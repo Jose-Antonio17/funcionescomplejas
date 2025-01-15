1ddb2a96f399b3196df05966c82822fb0ba7406e
Jose-Antonio17 <ceronortizjose@gmail.com>
2025-01-15 20:50:55 +0100

    miercolesfin

--- ClasesParesImpares/src/EjecucionTexto.java
+++ ClasesParesImpares/src/EjecucionTexto.java
@@ -1,7 +1,22 @@
 public class EjecucionTexto {
     public static void main(String[] args) {
-        String texto = "123456";
-        int suma = JuegoTextos.getSumaTexto(texto);
-        System.out.println(suma);
+        String isbn = "8441513929";
+        boolean validacion = JuegoTextos.validarISBN(isbn);
+        if(validacion){
+            System.out.println("ISBN correcto");
+        } else {
+            System.out.println("ISBN incorrecto");
+        }
+
+        String narcisista = "153";
+        validacion = JuegoTextos.narcisista(narcisista);
+        if(validacion){
+            System.out.println("Número narcisista");
+        } else {
+            System.out.println("Número no narcisista");
+        }
+        //String texto = "123456";
+        //int suma = JuegoTextos.getSumaTexto(texto);
+        //System.out.println(suma);
     }
 }
--- ClasesParesImpares/src/JuegoTextos.java
+++ ClasesParesImpares/src/JuegoTextos.java
@@ -8,4 +8,33 @@ public class JuegoTextos {
         }
         return suma;
     }
+
+    public static boolean validarISBN(String isbn) {
+        int suma = 0;
+        for (int i = 0; i < isbn.length(); i++) {
+            String numero = String.valueOf(isbn.charAt(i));
+            int numeroCovertido = Integer.parseInt(numero);
+            suma += (numeroCovertido*(i+1));
+        }
+        if (suma % 11 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static boolean narcisista (String num){
+        int numeroTexto = Integer.parseInt(num);
+        int suma = 0;
+        for (int i = 0; i < num.length(); i++) {
+            String numero = String.valueOf(num.charAt(i));
+            int numeroCovertido = Integer.parseInt(numero);
+            suma += ((int) Math.pow(numeroCovertido, 3));
+        }
+        System.out.println(suma);
+        if (numeroTexto == suma){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
